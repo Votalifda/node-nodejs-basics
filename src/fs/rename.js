3c6e0ccdb360eda1ca2c6877671e0a449690ba6f
Denis Filatov <denis.filatov@ideus.biz>
2024-01-28 01:46:44 +0200

    feat: rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,26 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const rename = async () => {
-    // Write your code here 
+    const __dirname = path.dirname(fileURLToPath(import.meta.url))
+    const wrongFile = path.join(__dirname, 'files', 'wrongFilename.txt');
+    const properFile = path.join(__dirname, 'files', 'properFilename.md');
+
+    const isFileExists = async (path) => {
+        try {
+            await fs.access(path);
+            return true;
+        } catch {
+            return false;
+        }
+    }
+
+    if (!await isFileExists(wrongFile) || await isFileExists(properFile)) {
+        throw new Error('FS operation failed');
+    } else {
+        await fs.rename(wrongFile, properFile);
+    }
 };
 
 await rename();
