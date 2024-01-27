3f79b3944eafbfa336aaa4121eb32b27f0659381
Denis Filatov <denis.filatov@ideus.biz>
2024-01-28 01:55:29 +0200

    feat: read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,17 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const read = async () => {
-    // Write your code here 
+    const __dirname = path.dirname(fileURLToPath(import.meta.url))
+    const fileToRead = path.join(__dirname, 'files', 'fileToRead.txt');
+    try {
+        await fs.access(fileToRead, fs.constants.F_OK);
+        const content = await fs.readFile(fileToRead, 'utf8');
+        console.log(content);
+    } catch {
+        throw new Error('FS operation failed');
+    }
 };
 
 await read();
