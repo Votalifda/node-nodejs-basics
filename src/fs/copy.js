869c5a78d42e87f2324788704b5c6e67468d762e
Denis Filatov <denis.filatov@ideus.biz>
2024-01-28 01:40:19 +0200

    feat: copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,32 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const copy = async () => {
-    // Write your code here 
+    const __dirname = path.dirname(fileURLToPath(import.meta.url))
+    const filesPath = path.join(__dirname, 'files');
+    const filesCopyPath = path.join(__dirname, 'files_copy');
+
+    const isDirExists = async (dir) => {
+        try {
+            await fs.access(dir);
+            return true;
+        } catch {
+            return false;
+        }
+    }
+
+    if (!await isDirExists(filesPath) || await isDirExists(filesCopyPath)) {
+        throw new Error('FS operation failed');
+    } else {
+        await fs.mkdir(filesCopyPath);
+        const files = await fs.readdir(filesPath);
+        for (const file of files) {
+            const from = path.join(filesPath, file);
+            const to = path.join(filesCopyPath, file);
+            await fs.copyFile(from, to);
+        }
+    }
 };
 
 await copy();
