c6855aabdc756b1140ab913a4fc540db1f2b2af3
Denis Filatov <denis.filatov@ideus.biz>
2024-01-28 01:53:18 +0200

    feat: list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,20 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const list = async () => {
-    // Write your code here 
+    const __dirname = path.dirname(fileURLToPath(import.meta.url))
+    const filesPath = path.join(__dirname, 'files');
+    try {
+        await fs.access(filesPath, fs.constants.F_OK);
+        const files = await fs.readdir(filesPath);
+        for (const file of files) {
+            console.log(file);
+        }
+    } catch {
+        throw new Error('FS operation failed');
+    }
+
 };
 
 await list();
