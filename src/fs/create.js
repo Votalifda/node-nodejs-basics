dbee101342f6a618cea05520eac2a26ab17f77d2
Denis Filatov <denis.filatov@ideus.biz>
2024-01-28 01:25:30 +0200

    feat: create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,15 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const create = async () => {
-    // Write your code here 
+    const __dirname = path.dirname(fileURLToPath(import.meta.url))
+    const fullPath = path.join(__dirname, 'files', 'fresh.txt');
+    try {
+        await fs.writeFile(fullPath, 'I am fresh and young', { flag: 'wx', encoding: 'utf-8' });
+    } catch {
+        throw new Error('FS operation failed');
+    }
 };
 
 await create();
