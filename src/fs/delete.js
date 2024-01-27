553a0020982fff35bc4f5683a82d8c1547b7274c
Denis Filatov <denis.filatov@ideus.biz>
2024-01-28 01:50:24 +0200

    feat: delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,16 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const remove = async () => {
-    // Write your code here 
+    const __dirname = path.dirname(fileURLToPath(import.meta.url))
+    const fileToRemove = path.join(__dirname, 'files', 'fileToRemove.txt');
+    try {
+        await fs.access(fileToRemove, fs.constants.F_OK);
+        await fs.unlink(fileToRemove);
+    } catch {
+        throw new Error('FS operation failed');
+    }
 };
 
 await remove();
