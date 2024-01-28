6608e1a031a7ea57b621f852d7617a56cda96535
Denis Filatov <denis.filatov@ideus.biz>
2024-01-28 02:04:41 +0200

    feat: env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,7 @@
 const parseEnv = () => {
-    // Write your code here 
+    const vars = Object.keys(process.env).filter(item => item.indexOf('RSS_') === 0);
+    const result = vars.map(item => `${item}=${process.env[item]}`).join('; ');
+    console.log(result)
 };
 
 parseEnv();
