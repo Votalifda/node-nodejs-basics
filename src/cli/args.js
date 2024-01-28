5af1d17aa788a0079fcbcd27bafb180202506ea6
Denis Filatov <denis.filatov@ideus.biz>
2024-01-28 02:19:10 +0200

    feat: args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2);
+    const result = args.reduce((acc, current, idx) => {
+        if (idx % 2 === 0) {
+            acc.push(`${current.replace('--', '')} is ${args[idx + 1]}`);
+        }
+        return acc;
+    }, []);
+    console.log(result.join(', '));
 };
 
 parseArgs();
