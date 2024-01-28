8eea03131bb9793e9a16ad5ed728bf3a4b1ef210
Denis Filatov <denis.filatov@ideus.biz>
2024-01-28 03:05:28 +0200

    feat: streams read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,20 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const read = async () => {
-    // Write your code here 
+    const __dirname = path.dirname(fileURLToPath(import.meta.url))
+    const fileToRead = path.join(__dirname, 'files', 'fileToRead.txt');
+    const stream = fs.createReadStream(fileToRead, { encoding: 'utf-8' });
+    let content = '';
+
+    stream.on('data', (data) => {
+        content += data;
+    });
+
+    stream.on('end', () => {
+        process.stdout.write(content);
+    });
 };
 
 await read();
