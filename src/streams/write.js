6cd97c69a787d93cd21c544a8cc52bcdc8973d11
Denis Filatov <denis.filatov@ideus.biz>
2024-01-28 03:14:00 +0200

    feat: streams write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,15 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const write = async () => {
-    // Write your code here 
+    const __dirname = path.dirname(fileURLToPath(import.meta.url))
+    const fileToWrite = path.join(__dirname, 'files', 'fileToWrite.txt');
+    const stream = fs.createWriteStream(fileToWrite);
+
+    process.stdin.on('data', (data) => {
+        stream.write(data);
+    })
 };
 
 await write();
