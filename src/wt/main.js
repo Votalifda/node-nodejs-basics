2408e7bf72dc78129705e8a4a1ac2f7bb1d6feeb
Denis Filatov <denis.filatov@ideus.biz>
2024-01-28 04:01:30 +0200

    feat: wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,24 @@
+import { Worker } from 'worker_threads';
+import os from 'os';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const performCalculations = async () => {
-    // Write your code here
+    const __dirname = path.dirname(fileURLToPath(import.meta.url))
+    const workerFile = path.join(__dirname, 'worker.js');
+    const promisesArr = [];
+    const cpus = os.cpus().length;
+
+    for (let i = 0; i < cpus; i++) {
+        const worker = new Worker(workerFile, { workerData: i + 10 });
+        const promise = new Promise((resolve) => {
+            worker.on('message', (result) => resolve(result));
+        });
+        promisesArr.push(promise);
+    }
+
+    const results = await Promise.all(promisesArr);
+    console.log(results);
 };
 
 await performCalculations();
